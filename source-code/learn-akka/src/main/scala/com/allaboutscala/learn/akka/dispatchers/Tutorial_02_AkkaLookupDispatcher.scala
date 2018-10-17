8e80cf0f2a6299f10922dad8afb0fff287f75a95
Nadim Bahadoor <nbahadoor@gmail.com>
2018-10-17 17:21:09 +0400

    Akka lookup dispatcher

---
+++ source-code/learn-akka/src/main/scala/com/allaboutscala/learn/akka/dispatchers/Tutorial_02_AkkaLookupDispatcher.scala
@@ -0,0 +1,31 @@
+package com.allaboutscala.learn.akka.dispatchers
+
+import akka.actor.ActorSystem
+
+/**
+  * Created by Nadim Bahadoor on 28/06/2016.
+  *
+  *  Tutorial: Learn How To Use Akka
+  *
+  * [[http://allaboutscala.com/scala-frameworks/akka/]]
+  *
+  * Copyright 2016 Nadim Bahadoor (http://allaboutscala.com)
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  * use this file except in compliance with the License. You may obtain a copy of
+  * the License at
+  *
+  *  [http://www.apache.org/licenses/LICENSE-2.0]
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  * License for the specific language governing permissions and limitations under
+  * the License.
+  */
+object Tutorial_02_AkkaLookupDispatcher extends App {
+
+  val system = ActorSystem("DonutStoreActorSystem")
+  implicit val executionContext = system.dispatchers.lookup("fixed-thread-pool")
+  system.terminate()
+}
